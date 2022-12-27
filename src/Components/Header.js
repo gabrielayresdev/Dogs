18f10ad1a5ed6e8a630fca172a5093ecee17c593
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2022-12-26 23:19:31 -0300

    useContext do login, permitindo o acesso as informações do usuários por todo o código

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,17 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { ReactComponent as Dogs } from '../Assets/dogs.svg';
 import styles from './Header.module.css';
+import { UserContext } from '../UserContext';
 
 const Header = () => {
+  const { data } = React.useContext(UserContext);
+
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
         <Link to="/" className={styles.logo}>
           <Dogs />
         </Link>
-        <Link to="/Login" className={styles.login}>
-          Login / Criar
-        </Link>
+        {data ? (
+          <Link to="/conta" className={styles.login}>
+            {data.nome}
+          </Link>
+        ) : (
+          <Link to="/Login" className={styles.login}>
+            Login / Criar
+          </Link>
+        )}
       </nav>
     </header>
   );
--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,43 +1,22 @@
 import React from 'react';
-import { TOKEN_POST, USER_GET } from '../../api.js';
 import useForm from '../../Hooks/useForm';
 import Button from '../Forms/Button';
 import Input from '../Forms/Input';
+import { UserContext } from '../../UserContext.js';
 
 const LoginForm = () => {
   const username = useForm();
   const password = useForm();
 
-  const { url, options } = TOKEN_POST({
-    username: username.value,
-    password: password.value,
-  });
-
-  async function getUser(token) {
-    const { url, options } = USER_GET(token);
-    const response = await fetch(url, options);
-    const json = await response.json();
-    console.log(json);
-  }
+  const { userLogin } = React.useContext(UserContext);
 
   async function handleSubmit(event) {
     event.preventDefault();
 
     if (username.validate() && password.validate()) {
-      const response = await fetch(url, options);
-      const json = await response.json();
-      console.log(json);
-      window.localStorage.setItem('token', json.token);
-      getUser(json.token);
+      userLogin(username.value, password.value);
     }
   }
-
-  React.useEffect(() => {
-    const token = window.localStorage.getItem('token');
-    if (token) {
-      getUser(token);
-    }
-  }, []);
 
   return (
     <section>
