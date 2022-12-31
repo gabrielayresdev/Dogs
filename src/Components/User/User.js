d2c37fe1e31b957cdfe1bb770d597e4930e0484e
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2022-12-31 20:07:41 -0300

    Header da página de usuário responsivo e funcional

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,7 +1,20 @@
 import React from 'react';
-
+import { Routes, Route } from 'react-router-dom';
+import UserHeader from './UserHeader';
+import UserPhotoPost from './UserPhotoPost';
+import Feed from '../Feed/Feed';
+import UserStats from './UserStats';
 const User = () => {
-  return <div>User</div>;
+  return (
+    <section className="container">
+      <UserHeader />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="postar" element={<UserPhotoPost />} />
+        <Route path="estatisticas" element={<UserStats />} />
+      </Routes>
+    </section>
+  );
 };
 
 export default User;
---
+++ src/Components/User/UserHeaderNav.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+import { UserContext } from '../../UserContext';
+import { ReactComponent as MinhasFotos } from '../../Assets/feed.svg';
+import { ReactComponent as Estatisticas } from '../../Assets/estatisticas.svg';
+import { ReactComponent as AdicionarFoto } from '../../Assets/adicionar.svg';
+import { ReactComponent as Sair } from '../../Assets/sair.svg';
+import styles from './UserHeaderNav.module.css';
+import useMedia from '../../Hooks/useMedia';
+
+const UserHeaderNav = () => {
+  const { userLogout } = React.useContext(UserContext);
+  const [mobileMenu, setMobileMenu] = React.useState(false);
+  const mobile = useMedia('(max-width: 40rem)');
+
+  const { pathname } = useLocation();
+  React.useEffect(() => setMobileMenu(false), [pathname]);
+
+  return (
+    <>
+      {mobile && (
+        <buttom
+          aria-label="Menu"
+          className={`${styles.mobileButton} ${
+            mobileMenu && styles.mobileButtonActive
+          }`}
+          onClick={() => setMobileMenu(!mobileMenu)}
+        ></buttom>
+      )}
+
+      <nav
+        className={`${mobile ? styles.navMobile : styles.nav} ${
+          mobileMenu && styles.navMobileActive
+        }`}
+      >
+        <NavLink to="/conta" end>
+          <MinhasFotos />
+          {mobile && 'Minhas Fotos'}
+        </NavLink>
+        <NavLink to="/conta/estatisticas">
+          <Estatisticas />
+          {mobile && 'Estatísticas'}
+        </NavLink>
+        <NavLink to="/conta/postar">
+          <AdicionarFoto />
+          {mobile && 'Adicionar Foto'}
+        </NavLink>
+        <button onClick={userLogout}>
+          <Sair />
+          {mobile && 'Sair'}
+        </button>
+      </nav>
+    </>
+  );
+};
+
+export default UserHeaderNav;
